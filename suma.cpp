356ef97908ec0d7b33b045b1fe17d01b4b8698fd
Danielht05 <00134125@uca.edu.sv>
2025-06-01 23:01:04 -0600

    Otra prueba

---
+++ suma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, suma;
+    
+    cout << "Digite dos numeros para sumarlos" << endl;
+    cin >> a;
+    cin >> b;
+
+    suma = a+b;
+
+    cout << "La suma de los numeros es: " << suma << endl;
+
+    return 0;
+}
